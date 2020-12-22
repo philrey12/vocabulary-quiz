acff0706fda3975326b344393e409bbba665926f
philrey12 <phil.raquinel@gmail.com>
2020-12-22 11:28:47 +0800

    Add questions.js

---
+++ js/questions.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+        word: 'redact',
+        option1: 'expect',
+        option2: 'edit',
+        option3: 'release',
+        option4: 'reply',
+        answer: '2'
+    },
+    {
+        word: 'recidivism',
+        option1: 'relapse',
+        option2: 'instability',
+        option3: 'sadness',
+        option4: 'uncertainty',
+        answer: '1'
+    },
+    {
+        word: 'render',
+        option1: 'label',
+        option2: 'measure',
+        option3: 'deliver',
+        option4: 'list',
+        answer: '3'
+    },
+    {
+        word: 'prudence',
+        option1: 'charity',
+        option2: 'wisdom',
+        option3: 'talent',
+        option4: 'confusion',
+        answer: '2'
+    },
+    {
+        word: 'ludicrous',
+        option1: 'laughable',
+        option2: 'shameful',
+        option3: 'believable',
+        option4: 'insulting',
+        answer: '1'
+    }
+];
